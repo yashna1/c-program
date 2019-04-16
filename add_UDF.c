f41c2ea729635215a68886fd8be99bfd0fff19ed
yashna1 <49425003+yashna1@users.noreply.github.com>
2019-04-16 09:28:14 +0530

    Create add_UDF.c

---
+++ add_UDF.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void 
+void getinput()
+{
+int a,b,c;
+printf(“enter the value of a:\n”);
+scanf(“%d”,&a);
+printf(“enter the value of b:\n”);
+scanf(“%d”,&b);
+int sum()
+{
+c=a+b;
+}
+int output()
+{
+sum()
+}
+return;
+}
